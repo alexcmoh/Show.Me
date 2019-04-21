6c1164cc9adbba0c3e9c8c03071a15e90df21e77
Roboloid <roboloidx@gmail.com>
2019-04-21 15:30:18 -0400

    Added app.js

---
+++ public/app.js
@@ -0,0 +1,32 @@
+//(function() {
+	// Initialize Firebase
+	var config = {
+		apiKey: "AIzaSyAYAf35t-8bGqp-6WsRtSM-sqJqp5z9P_w",
+		authDomain: "showme-22fa9.firebaseapp.com",
+	    databaseURL: "https://showme-22fa9.firebaseio.com",
+		projectId: "showme-22fa9",
+		storageBucket: "showme-22fa9.appspot.com",
+		messagingSenderId: "336455638979"
+	};
+	firebase.initializeApp(config);
+	var firestore = firebase.firestore();
+	
+	const docRef = firestore.collection("users").doc("person");
+	const userTextField = document.querySelector("#uname");
+	const passTextField = document.querySelector("#psw");
+	
+	saveButton.addEventListener("enter", function() {
+		const userSave = userTextField.value;
+		const passSave = passTextField.value;
+		docRef.set({
+			password: passSave
+			username: userSave
+		}).then(function() {
+			console.log("Username and Password saved!");
+		}).catch(function (error) {
+			console.log("Error received: ", error);
+		});
+	})
+	
+//})();
+  
